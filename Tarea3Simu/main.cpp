c557a794a2ab14788d8b562befa49a2851b7856d
RoxanaCortez <00502211@uca.edu.sv>
2020-04-18 01:40:24 -0600

    Eliminando nombre de la ecuacion y listo

--- Tarea3Simu/main.cpp
+++ Tarea3Simu/main.cpp
@@ -15,7 +15,7 @@ int main(){
     Vector U;
 
     cout << "IMPLEMENTACI"<<char(224)<<"N DEL M"<<char(144)<<"TODO DE LOS ELEMENTOS FINITOS\n"
-         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 1 DIMENSI"<<char(224)<<"N\n"
+         << "\t- 1 DIMENSI"<<char(224)<<"N\n"
          << "\t- FUNCIONES DE FORMA LINEALES\n" << "\t- PESOS DE GALERKIN\n"
          << "*********************************************************************************\n\n";
 
